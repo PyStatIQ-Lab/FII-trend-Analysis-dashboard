9160457a9befa10912071e082a35d27561aed7e9
PyStatIQ-Lab <support@pystatiq.com>
2025-04-09 10:24:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ API_URL = "https://oxide.sensibull.com/v1/compute/cache/fii_dii_daily"
 CACHE_EXPIRY_DAYS = 1  # Refresh cache every day
 
 # Load data function with caching
-@st.cache_data(ttl=timedelta(days=CACHE_EXPIRY_DAYS).clear()
-# Load data function with caching
 @st.cache_data(ttl=timedelta(days=CACHE_EXPIRY_DAYS))
 def load_data():
     try:
